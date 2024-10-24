feec11d8067376507cc480f96e6c6f91db45a158
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-10-24 19:01:50 +0300

    enabled CORS

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ notes = [
     }
 ]
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
     res.send('Works!')
 })
