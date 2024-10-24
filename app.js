01a91bd05de0f26f2752c418eedd9ee0875534a1
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-10-24 18:36:28 +0300

    added basic endpoints for getting notes

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// temporary notes (to be replaced with database later)
+notes = [
+    {
+        id: 1,
+        title: 'Note #1',
+        content: 'Note content for note one...'
+    },
+    {
+        id: 2,
+        title: 'Note #2',
+        content: 'Note content for note two...'
+    }
+]
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Works!')
+})
+
+
+app.get('/api/get-notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/get-note/:id', (req, res) => {
+    id = req.params.id
+    note = notes.find(note => note.id == id)
+    res.json(note)
 })
 
 app.listen(port, () => {
